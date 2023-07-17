3e835cb705c7ef1f422b5b202ee35059b12c9630
divo <divoapple@gmail.com>
2023-07-17 10:23:46 +0100

    Initial Commit

---
+++ DynamicPlaner/DynamicPlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicPlanerApp.swift
+//  DynamicPlaner
+//
+//  Created by Steven Diviney on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DynamicPlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
