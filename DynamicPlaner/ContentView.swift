ab03168377c7fc42878dc71a687a0db1880351a7
divo <divoapple@gmail.com>
2023-07-25 15:17:37 +0100

    POC for casting while preserving binding

--- DynamicPlaner/ContentView.swift
+++ DynamicPlaner/ContentView.swift
@@ -13,32 +13,38 @@ import SwiftUI
 // could live in a big ViewBuilder function too.
 // TODO: Abstract or something else? I need an abstract class that defines a text function, and leave
 // everything else up to the subclasses. This is all very fucking annoying
-class ViewModel {
+class BaseModel: ObservableObject {
+  func toString() -> String { "" }
+}
+
+class TextViewModel: BaseModel {
   @Published var text: String
-  @Published var done: Bool
   let weight: Int
-  let type: ViewType //TODO: Push this information into subclasses(?)
-  
-  enum ViewType {
-    case text
-    case textField
-    case checkBox
-  }
-  
-  init(text: String = "View Model", type: ViewType, weight: Int = 1, done: Bool = false) {
+
+  init(text: String = "View Model", weight: Int = 1) {
     self.text = text
-    self.type = type
     self.weight = weight
-    self.done = done
   }
   
-  func toString() -> String {
+  override func toString() -> String {
+    text
+  }
+}
+
+class TextFieldModel: BaseModel {
+  @Published var text: String
+
+  init(text: String = "") {
+    self.text = text
+  }
+  
+  override func toString() -> String {
     text
   }
 }
 
 struct TextView: View {
-  @Binding var state: String
+  var state: String
   var weight: Int = 1
   
   var body: some View {
@@ -82,59 +88,72 @@ struct CheckBoxView: View  {
   }
 }
 
+class ViewModel: ObservableObject {
+  @Published var models: [BaseModel] = []
+}
+
 struct ContentView : View {
   @State var stateString = "# This is some text\n \n# And some more text"
-  @State var vm: [ViewModel] = []
+  @StateObject var vm = ViewModel()
+  //  @StateObject var vm: [ViewModel] = []
   
-  @ViewBuilder func render(vm: Binding<ViewModel>) -> some View {
-    switch vm.wrappedValue.type {
-    case .text:
-      TextView(state: vm.text, weight: vm.wrappedValue.weight)
-    case .textField:
-      TextFieldView(state: vm.text)
-    case .checkBox:
-      CheckBoxView(text: vm.text, done: vm.done)
+  @ViewBuilder func render(vm: Binding<BaseModel>) -> some View {
+    switch vm {
+    case let textVm as TextViewModel:
+      TextView(state: textVm.text, weight: textVm.weight)
+      //      TextView(state: textVm.$text, weight: textVm.weight)
+//    case let textFieldVm as TextFieldModel:
+//      TextFieldView(state: textFieldVm.text)
+    default:
+      Spacer()
     }
   }
   
-  // 2
   var body: some View {
     VStack {
       TextEditor(text: $stateString)
       
       Button("Serialize") {
-        print(vm.map({ vm in
+        print(vm.models.map({ vm in
           vm.toString()
         }))
       }
       
       List {
-        ForEach(0..<$vm.count, id: \.self) { element in
-          render(vm: $vm[element])
+        ForEach(0..<$vm.models.count, id: \.self) { element in
+          //          render(vm: $vm.models[element])
+          if let textVm = vm.models[element] as? TextViewModel {
+            TextView(state: textVm.text, weight: textVm.weight)
+          } else if vm.models[element] is TextFieldModel {
+            var fieldVm = vm.models[element] as! TextFieldModel
+            let bd = Binding<TextFieldModel>(get: { fieldVm }, set: { fieldVm = $0 })
+            TextFieldView(state: bd.text)
+          }
         }
       }
       
     }.onAppear {
-      vm = deserialize(state: stateString)
+      vm.models = deserialize(state: stateString)
     }
     .onChange(of: stateString, perform: { newValue in
-      vm = deserialize(state: stateString)
+      vm.models = deserialize(state: stateString)
     })
   }
   
-  func deserialize(state: String) -> [ViewModel] {
-    var result: [ViewModel] = []
+  func deserialize(state: String) -> [BaseModel] {
+    var result: [BaseModel] = []
     state.split(separator: "\n").map { string in
       let tokens = String(string).split(separator: " ") // Has to be better way than this nonsense
       if string.first == "#" {
         let text = String(string.drop(while: { c in c == "#" }))
-        result.append(ViewModel(text: text, type: .text, weight: tokens.first?.count ?? 1))
+//        result.append(ViewModel(text: text, type: .text, weight: tokens.first?.count ?? 1))
+        result.append(TextViewModel(text: text, weight: tokens.first?.count ?? 1))
       } else if(string.first == " ") {
-        result.append(ViewModel(text: "", type: .textField))
+        result.append(TextFieldModel())
       } else if(string.first == "-" && string.count > 4) {
         // -[x]
         let text = String(string.dropFirst(5))
-        result.append(ViewModel(text: text, type: .checkBox, done: false))
+//        result.append(ViewModel(text: text, type: .checkBox, done: false))
       }
     }
     
