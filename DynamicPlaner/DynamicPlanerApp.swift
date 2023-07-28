74f74caa71639e66a1192b820e5949f1b795445b
divo <divoapple@gmail.com>
2023-07-28 14:59:48 +0100

    Store documents in iCloud

--- DynamicPlaner/DynamicPlanerApp.swift
+++ DynamicPlaner/DynamicPlanerApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct DynamicPlanerApp: App {
   init() {
+   FileUtil.setDriveURL()
    if UserDefaults().string(forKey: Constants.templateKey) == nil {
       UserDefaults().set(Constants.defaultTemplate, forKey: Constants.templateKey)
     }
