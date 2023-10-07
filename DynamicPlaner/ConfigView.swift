09bcf48e1a87f4868a8c744ff95bd74e36c7cac3
divo <divoapple@gmail.com>
2023-10-07 21:48:00 +0100

    Add a simple legend for markdown template
    Looks like shit

--- DynamicPlaner/ConfigView.swift
+++ DynamicPlaner/ConfigView.swift
@@ -1,12 +1,25 @@
 import SwiftUI
 
+struct LegendItem: View {
+  let name: String
+  let text: String
+  
+  var body: some View {
+    HStack {
+      Text(text)
+      Spacer()
+      Text(name)
+    }
+  }
+}
+
 struct ConfigView: View {
   @ObservedObject var viewModel: ConfigViewModel
   @State var dayStart = UserDefaults().object(forKey: "dayStart") as? Date ?? Date.now
   @State var planningTime = UserDefaults().object(forKey: "planningTime") as? Date ?? Date.now
   @State var startNotification = false
   @State var planningNotification = false
-  
+
   var body: some View {
     List {
       HStack {
@@ -48,7 +61,18 @@ struct ConfigView: View {
         }
       
       VStack {
-        TextView(text: "Template:")
+        VStack {
+          Text("Legend")
+          LegendItem(name: "Heading", text: "#")
+          LegendItem(name: "Checkbox", text: "- [ ] ")
+          LegendItem(name: "Text field", text: "\\n")
+          LegendItem(name: "Multi line text field", text: "\\n\\n")
+          LegendItem(name: "Reminder", text: "[label](time)")
+          LegendItem(name: "Create empty checkbox", text: "+")
+        }.background(Color(uiColor: UIColor.systemGray6))
+            
+
+        TextView(text: "Template:").padding([.top], 10)
         ScrollView {
           ZStackLayout(alignment: .topLeading) {
             Text(viewModel.planningTemplate) // This is the hack to get the TextEditor to grow AND shrink as text is added .
